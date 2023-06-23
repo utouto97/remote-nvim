f629e07c8af2ae64a7c05767f3927e7a1e98a4ad
utouto97 <utouto97@gmail.com>
2023-06-24 01:14:25 +0900

    update installation of neovim

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 if [ "$(uname)" = "Linux" ]; then
-  if [ -z $(command -v wget) ] || [ -z $(command -v tar) ]; then
-    if [ -n "$(command -v apt)" ]; then
-      apt update -y
-      apt install -y wget tar
-    fi
-  fi
-  if [ -z $(command -v git) ]; then
-    if [ -n "$(command -v apk)" ]; then
+  if [ -n "$(command -v apk)" ]; then
+    # alpine
+
+    if [ -z $(command -v git) ]; then
       apk add git
     fi
-  fi
 
-  filename="nvim-linux64.tar.gz"
-  version="stable"
-  cd /tmp
-  wget "https://github.com/neovim/neovim/releases/download/${version}/${filename}"
-  tar zxf "${filename}"
-  cp nvim-linux64/bin/nvim /usr/local/bin/
-  cp -R nvim-linux64/share/* /usr/local/share/
+    apk add neovim
+  else
+    if [ -z $(command -v wget) ] || [ -z $(command -v tar) ]; then
+      if [ -n "$(command -v apt)" ]; then
+        apt update -y
+        apt install -y wget tar
+      fi
+    fi
+
+    filename="nvim-linux64.tar.gz"
+    version="stable"
+    cd /tmp
+    wget "https://github.com/neovim/neovim/releases/download/${version}/${filename}"
+    tar zxf "${filename}"
+    cp nvim-linux64/bin/nvim /usr/local/bin/
+    cp -R nvim-linux64/share/* /usr/local/share/
+  fi
 fi
